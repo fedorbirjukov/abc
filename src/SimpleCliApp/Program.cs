fac4eb4560622c49c2290e6bc0e31171195d0cd0
Ted <fedor.birjukov@gmail.com>
2025-06-06 19:32:02 +0000

    Add simple C# CLI application

---
+++ src/SimpleCliApp/Program.cs
@@ -0,0 +1,8 @@
+﻿// A simple C# CLI application
+Console.WriteLine("Welcome to my Simple CLI App!");
+Console.Write("Please enter your name: ");
+string? name = Console.ReadLine();
+Console.WriteLine($"Hello, {name}! Nice to meet you.");
+
+Console.WriteLine("\nPress any key to exit...");
+Console.ReadKey();
